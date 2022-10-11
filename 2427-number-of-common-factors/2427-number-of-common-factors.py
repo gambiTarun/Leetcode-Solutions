ba5a831cc9512f6b4979974056c6ec5de71bb762
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-11 11:20:20 -0700

    Time: 50 ms (67.16%), Space: 13.8 MB (62.88%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -2,7 +2,7 @@ class Solution:
     def commonFactors(self, a: int, b: int) -> int:
         
         c,res = 1,0
-        m = min(a,b)
+        m = gcd(a,b)
         while c<=1000 and c<=m:
             if a%c==0 and b%c==0: res+=1
             c+=1
