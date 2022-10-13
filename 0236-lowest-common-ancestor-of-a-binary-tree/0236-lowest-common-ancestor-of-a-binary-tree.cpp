4043331cf37674969717b6e9901f7d92809a93c7
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-13 12:46:50 -0700

    Time: 147 ms (42.66%), Space: 18.1 MB (98.51%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        def dfs(n,p,q):
+            if not n:
+                return None
+            
+            pinleft = covers(n.left,p)
+            qinleft = covers(n.left,q)
+
+            if pinleft^qinleft: return n
+            
+            return dfs(n.left,p,q) if pinleft else dfs(n.right,p,q)
+            
+        def covers(p,q):
+            if not p:
+                return False
+            if p==q: return True
+            return covers(p.left,q) or covers(p.right,q)
+        
+        if covers(p,q): return p
+        if covers(q,p): return q
+        return dfs(root,p,q)
