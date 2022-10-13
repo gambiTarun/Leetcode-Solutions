3d2af8b9c884683f242b98e493407ea8f3912adb
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-13 13:11:57 -0700

    Time: 213 ms (8.26%), Space: 26.3 MB (30.75%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,23 +8,30 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        def dfs(n,p,q):
-            if not n:
-                return None
+#         def dfs(n,p,q):
+#             if not n:
+#                 return None
             
-            pinleft = covers(n.left,p)
-            qinleft = covers(n.left,q)
+#             pinleft = covers(n.left,p)
+#             qinleft = covers(n.left,q)
 
-            if pinleft^qinleft: return n
+#             if pinleft^qinleft: return n
             
-            return dfs(n.left,p,q) if pinleft else dfs(n.right,p,q)
+#             return dfs(n.left,p,q) if pinleft else dfs(n.right,p,q)
             
-        def covers(p,q):
-            if not p:
-                return False
-            if p==q: return True
-            return covers(p.left,q) or covers(p.right,q)
+#         def covers(p,q):
+#             if not p:
+#                 return False
+#             if p==q: return True
+#             return covers(p.left,q) or covers(p.right,q)
         
-        if covers(p,q): return p
-        if covers(q,p): return q
-        return dfs(root,p,q)
+#         if covers(p,q): return p
+#         if covers(q,p): return q
+#         return dfs(root,p,q)
+
+        if root in (None, p, q): return root
+    
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        
+        return root if left and right else (left if left else right)
