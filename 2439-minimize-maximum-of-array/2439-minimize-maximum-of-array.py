31b84658140ab8d7e00deb7c6a548f2c23f7f974
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-21 16:05:26 -0700

    Time: 1622 ms (74.38%), Space: 26.7 MB (72.98%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -6,8 +6,8 @@ class Solution:
         while l<=r:
             m = l+(r-l)//2
             
-            if all(v<=m*(i+1) for i,v in enumerate(accumulate(nums))):  r = m-1
-            else:   l = m+1
+            if any(v>m*(i+1) for i,v in enumerate(accumulate(nums))):  l = m+1
+            else:   r = m-1
                 
         return l
                 
