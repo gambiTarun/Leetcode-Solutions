3c1c594b9554993504abd21cf9e18a312a6055f1
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-21 16:06:47 -0700

    Time: 1633 ms (74.17%), Space: 26.8 MB (72.27%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,16 +1,16 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
         
-#         l, r = 0,max(nums)
+        l, r = 0,max(nums)
         
-#         while l<=r:
-#             m = l+(r-l)//2
+        while l<=r:
+            m = l+(r-l)//2
             
-#             if any(v>m*(i+1) for i,v in enumerate(accumulate(nums))):  l = m+1
-#             else:   r = m-1
+            if any(v>m*(i+1) for i,v in enumerate(accumulate(nums))):  l = m+1
+            else:   r = m-1
                 
-#         return l
+        return l
 
 
-        return max(ceil(v/(i+1)) for i,v in enumerate(accumulate(nums)))
+        # return max(ceil(v/(i+1)) for i,v in enumerate(accumulate(nums)))
                 
