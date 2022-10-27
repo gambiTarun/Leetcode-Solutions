5cd680589ba91b284f90984c49c25d3605789f9e
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-27 13:27:50 -0700

    Time: 33 ms (96.50%), Space: 14 MB (82.39%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,19 +1,26 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
-        def bintoset(x):
-            tmp = []
-            i = 0
-            while x:
-                if x & 1:
-                    tmp.append(nums[i])
-                i += 1
-                x >>= 1
+#         def bintoset(x):
+#             tmp = []
+#             i = 0
+#             while x:
+#                 if x & 1:
+#                     tmp.append(nums[i])
+#                 i += 1
+#                 x >>= 1
                 
-            return tmp
+#             return tmp
         
-        res = []
-        for i in range((1<<len(nums))):
-            res.append(bintoset(i))
+#         res = []
+#         for i in range((1<<len(nums))):
+#             res.append(bintoset(i))
+            
+#         return res
+
+        res = [[]]
+        
+        for i in nums:
+            res += [x+[i] for x in res]
             
         return res
