26eafa78fcd044789161124a9a4c6dd3cfe8bd8f
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-27 16:06:40 -0700

    Time: 5339 ms (60.15%), Space: 14.8 MB (71.70%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.py
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.py
@@ -4,7 +4,7 @@ class Solution:
         dp = [[0]*(n+1) for _ in range(n+1)]
         
         for i in range(n,-1,-1):
-            for j in range(i,n+1):
+            for j in range(i+1,n+1):
                 dp[i][j] = min([max(dp[i][x-1],dp[x+1][j])+x for x in range(i,j)], default=0)
         
         return dp[0][n]
