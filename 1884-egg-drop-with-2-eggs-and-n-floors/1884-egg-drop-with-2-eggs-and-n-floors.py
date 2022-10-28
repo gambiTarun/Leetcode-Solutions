472627c21e11447afe7a2524e530234bb5fcc363
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-27 17:54:32 -0700

    Time: 4486 ms (25.97%), Space: 16.8 MB (5.52%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoEggDrop(self, n: int) -> int:
+        
+#         dp = [[0]*(n+1) for _ in range(n+1)]
+        
+#         for i in range(n,-1,-1):
+#             for j in range(i,n+1):
+#                 dp[i][j] = min([max(x-i,dp[x+1][j])+1 for x in range(i,j)], default=0)
+        
+#         return dp[0][n]
+        
+        @cache
+        def eggs(f):
+            
+            return min([1+max(i-1, eggs(f-i)) for i in range(1,f)], default=1)
+        
+        return eggs(n)
