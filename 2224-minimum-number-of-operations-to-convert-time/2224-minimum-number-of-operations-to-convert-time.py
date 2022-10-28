a59e9462124a2881fa21fcd78869d9c4a78271ff
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-28 14:47:12 -0700

    Time: 282 ms (5.14%), Space: 13.9 MB (26.00%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
@@ -2,24 +2,36 @@ class Solution:
     def convertTime(self, current: str, correct: str) -> int:
         
         
-        def check(curr):
-            if curr==corrm: return 0
+#         def check(curr):
+#             if curr==corrm: return 0
             
-            if curr+60<=corrm:
-                return check(curr+60)+1 
-            elif curr+15<=corrm:
-                return check(curr+15)+1 
-            elif curr+5<=corrm:
-                return check(curr+5)+1 
+#             if curr+60<=corrm:
+#                 return check(curr+60)+1 
+#             elif curr+15<=corrm:
+#                 return check(curr+15)+1 
+#             elif curr+5<=corrm:
+#                 return check(curr+5)+1 
             
-            return check(curr+1)+1 
+#             return check(curr+1)+1 
             
             
-                
         
+#         currm = sum([int(t)*m for t,m in zip(current.split(":"),[60,1])])
+#         corrm = sum([int(t)*m for t,m in zip(correct.split(":"),[60,1])])
+        
+        
+#         return check(currm)
+
         currm = sum([int(t)*m for t,m in zip(current.split(":"),[60,1])])
         corrm = sum([int(t)*m for t,m in zip(correct.split(":"),[60,1])])
+        tar = corrm-currm
         
-        
-        return check(currm)
+        dp = [1441]*(tar+1)
+        dp[0] = 0
+        for i in range(tar+1):
+            for j in [60,15,5,1]:
+                if i>=j:
+                    dp[i] = min(dp[i],dp[i-j]+1)
+                    
+        return dp[tar]
         
