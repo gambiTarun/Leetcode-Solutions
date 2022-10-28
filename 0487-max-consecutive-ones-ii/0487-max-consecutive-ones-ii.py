0d217b9fbf29005c0d8a776199bc164f8cbb15be
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-28 13:58:20 -0700

    Time: 409 ms (92.36%), Space: 14.3 MB (70.97%) - LeetHub

--- 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.py
+++ 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.py
@@ -3,15 +3,12 @@ class Solution:
         
         l,r,z = 0,0,-1
         res = 0
-        mp = {}
         while r<len(nums):
             if nums[r]==0:
                 if z!=-1: 
                     l = z+1
-                
                 z = r
             
-                
             res = max(res,r-l+1)
                 
             r += 1
