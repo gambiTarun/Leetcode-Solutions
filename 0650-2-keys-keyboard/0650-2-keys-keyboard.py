34bffdf14cfc1852b418f602c1f4d809b424aec4
Tarunbir Gambhir <tarun0131@gmail.com>
2022-10-28 12:54:48 -0700

    Time: 43 ms (91.81%), Space: 13.9 MB (43.94%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -5,4 +5,4 @@ class Solution:
         if n==1:
             return 0
             
-        return min([i + self.minSteps(n//i) for i in range(2,n) if n%i==0], default=n)
+        return min([i + self.minSteps(n//i) for i in range(2,int(n**0.5) + 1) if n%i==0], default=n)
