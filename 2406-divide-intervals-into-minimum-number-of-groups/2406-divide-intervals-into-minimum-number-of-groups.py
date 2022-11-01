ba199a80164a75da00e8ad0e21e3b6d2c61b5561
Tarunbir Gambhir <tarun0131@gmail.com>
2022-11-01 00:12:29 -0700

    Time: 3538 ms (46.45%), Space: 52.9 MB (98.18%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -1,16 +1,11 @@
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
         
-        intr = [0]*(max(map(max,intervals))+2)
+        mh = []
         
-        for i in intervals:
-            intr[i[0]-1] += 1
-            intr[i[1]] -= 1
-            
-        res = intr[0]
-        for i in range(1,len(intr)):
-            intr[i] += intr[i-1]
-            res = max(res, intr[i])
+        for i in sorted(intervals):
+            if mh and mh[0]<i[0]:
+                heappop(mh)
+            heappush(mh,i[1])
         
-        
-        return res
+        return len(mh)
