5b0e7190413b858cfffdd7458608d6ebf767d99c
Tarunbir Gambhir <tarun0131@gmail.com>
2022-12-17 03:23:33 -0800

    Time: 76 ms (68.40%), Space: 13.8 MB (97.12%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        
+        for i in range(1,n+1):
+            if i*(i+1)/2 == n*(n+1)/2 - i*(i-1)/2:
+                return i
+            
+        return -1
