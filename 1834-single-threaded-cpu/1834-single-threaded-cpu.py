94a64c3825d018d16d186660c79a683277aa62b8
Tarunbir Gambhir <tarun0131@gmail.com>
2022-12-23 14:32:03 +0530

    Time: 2126 ms (82.85%), Space: 63.3 MB (17.31%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -0,0 +1,23 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        
+        alltasks = sorted([(t[0],t[1],i) for i,t in enumerate(tasks)])
+        print(alltasks)
+        nexttask = []
+        ans = []
+        i = 0
+        ctime = alltasks[i][0]
+        
+        while len(ans)<len(alltasks):
+            while i<len(tasks) and ctime>=alltasks[i][0]:
+                heapq.heappush(nexttask, (alltasks[i][1],alltasks[i][2]))
+                i+=1
+                
+            if nexttask:
+                t, idx = heapq.heappop(nexttask)
+                ctime+=t
+                ans.append(idx)
+            else:
+                ctime = alltasks[i][0]
+                
+        return ans
