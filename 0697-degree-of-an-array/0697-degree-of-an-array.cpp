15b55133def19f0d0dbe15f54cfc412af6419a30
Tarunbir Gambhir <tarun0131@gmail.com>
2023-01-24 19:03:57 -0800

    Time: 119 ms (21.75%), Space: 26.1 MB (46.95%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        unordered_map<int,vector<int>> mp;
+        int res=1,reslen=1;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i])==mp.end())
+                mp[nums[i]] = {1,i,i};
+            else{
+                mp[nums[i]][0]++;
+                mp[nums[i]][2]=i;
+            }
+            int len=mp[nums[i]][2]-mp[nums[i]][1]+1;
+            if(res<mp[nums[i]][0] || (res==mp[nums[i]][0] && reslen>len)){
+                res = mp[nums[i]][0];
+                reslen = len;
+            }
+            // cout<<res<<","<<reslen<<endl;
+        }
+        
+        for(auto x:mp){
+            cout<<x.first<<":"<<x.second[0]<<","<<x.second[1]<<","<<x.second[2]<<endl;
+        }
+        return reslen;
+    }
+};
