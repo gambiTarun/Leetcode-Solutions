c723d0e76301c9e01713076dda13f111634111c0
Tarunbir Gambhir <tarun0131@gmail.com>
2023-02-05 19:13:30 -0800

    Time: 3 ms (43.20%), Space: 6 MB (63.51%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        
+        while(b){
+            int c = a&b;
+            a = a^b;
+            b = (unsigned)c << 1;
+        }
+        return a;
+    }
+};
