509e0f3d6a6e764f6da1bce0969ed0c18cda1aff
Tarunbir Gambhir <tarun0131@gmail.com>
2023-02-11 15:00:03 -0800

    Time: 32 ms (71.28%), Space: 25 MB (76.98%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> res(encoded.size()+1,first);
+        for(int i=0;i<encoded.size();i++){
+            res[i+1]=encoded[i]^res[i];
+        }
+        return res;
+    }
+};
