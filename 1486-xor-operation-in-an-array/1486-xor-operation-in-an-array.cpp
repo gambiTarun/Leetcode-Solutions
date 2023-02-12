5714842a4f9050dce8124bc4fe430cd070947c9f
Tarunbir Gambhir <tarun0131@gmail.com>
2023-02-12 13:18:35 -0800

    Time: 0 ms (100.00%), Space: 6 MB (71.02%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -3,7 +3,7 @@ public:
     int xorOperation(int n, int start) {
         int res=0;
         for(int i=0;i<n;i++){
-            res^=start+2*i;
+            res^=start+(i<<1);
         }
         return res;
     }
