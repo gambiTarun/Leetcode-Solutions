d268f5006e0cb0a076d2450b90269a321140d23d
Tarunbir Gambhir <tarun0131@gmail.com>
2023-03-11 12:45:25 -0800

    Time: 8 ms (5.91%), Space: 7.7 MB (5.68%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -3,15 +3,23 @@ public:
     void gameOfLife(vector<vector<int>>& board) {
         int m = board.size();
         int n = board[0].size();
-        vector<vector<int>> prev=board;
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 // cout<<i<<","<<j<<endl;
-                board[i][j]=alive(prev,i,j)?1:0;
+                alive(board,i,j);
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]<0)
+                    board[i][j]=1;
+                else if(board[i][j]>1)
+                    board[i][j]=0;
             }
         }
     }
-    bool alive(vector<vector<int>>&b, int i, int j){
+    void alive(vector<vector<int>>&b, int i, int j){
         vector<int> dirs[]={{1,1},{1,-1},{-1,1},{-1,-1},{0,-1},{0,1},{1,0},{-1,0}};
         int n=0;
         for(auto d:dirs){
@@ -19,10 +27,16 @@ public:
             int ni = i+d[0];
             int nj = j+d[1];
             // cout<<ni<<","<<nj<<endl;
-            if(ni>=0 && nj>=0 && ni<b.size() && nj<b[0].size() && b[ni][nj])
+            if(ni>=0 && nj>=0 && ni<b.size() && nj<b[0].size() && b[ni][nj]>0)
                 n++;
         }
         // cout<<n<<endl;
-        return ((b[i][j]==1&&n>1&&n<4)||(b[i][j]==0&&n==3));
+        if(b[i][j]==1 && n>3)
+            b[i][j]++;
+        if(b[i][j]==1 && n<2)
+            b[i][j]++;
+        if(b[i][j]==0 && n==3)
+            b[i][j]--;
+            
     }
 };
