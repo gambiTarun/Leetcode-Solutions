e4c370b18604e41f5ec0cee8cffe9f01c60673dc
Tarunbir Gambhir <tarun0131@gmail.com>
2023-03-15 14:48:00 -0700

    Time: 447 ms (86.40%), Space: 165.1 MB (86.61%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,62 +1,33 @@
-class Node{
-public:
-    int key,value;
-    Node *prev,*next;
-    Node(int k, int v, Node* p, Node* n)
-        :key(k),value(v),prev(p),next(n)
-    {
-    }
-    
-    
-};
 class LRUCache {
 public:
-    Node *start,*end;
-    unordered_map<int,Node*> mp;
+    unordered_map<int, list<pair<int,int>>::iterator> mp;
+    list<pair<int,int>> dll;
     int size;
     LRUCache(int capacity) {
-        start = new Node(-1,-1,NULL,NULL);
-        end = new Node(-1,-1,start,NULL);
-        start->next = end;
-        size = capacity;
+        size=capacity;
     }
     
     int get(int key) {
         if(mp.find(key)==mp.end())
             return -1;
-        
-        // remove from curr position
-        mp[key]->prev->next = mp[key]->next;
-        mp[key]->next->prev = mp[key]->prev;
-        
-        // insert at the head
-        mp[key]->next = start->next;
-        start->next->prev = mp[key];
-        mp[key]->prev = start;
-        start->next = mp[key];
-        
-        return mp[key]->value;
+        dll.splice(dll.begin(),dll,mp[key]);
+        return mp[key]->second;
     }
     
     void put(int key, int value) {
         if(mp.find(key)!=mp.end()){
-            mp[key]->value = value;
-            get(key);
+            dll.splice(dll.begin(),dll,mp[key]);
+            mp[key]->second = value;
             return;
         }
         
         if(mp.size()==size){
-            Node* del = end->prev;
-            mp.erase(del->key);
-            end->prev = del->prev;
-            del->prev->next = end;
-            delete del;
+            mp.erase(dll.rbegin()->first);
+            dll.pop_back();
         }
         
-        mp[key] = new Node(key,value,start,start->next);
-        start->next->prev = mp[key];
-        start->next = mp[key];
-        
+        dll.emplace_front(key,value);
+        mp[key] = dll.begin();
     }
 };
 
