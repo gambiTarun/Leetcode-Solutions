488cf297943a59f25a3f1ae32f2451393fc350ea
Tarunbir Gambhir <tarun0131@gmail.com>
2023-03-31 21:03:04 -0700

    Time: 3 ms (99.70%), Space: 21.7 MB (56.24%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,31 +9,21 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution{
+private:
+    TreeNode* prev=NULL;
 public:
-    bool isValidBST(TreeNode* root) {
+    bool isValidBST(TreeNode* root){
         if(!root)
             return true;
         
-        if(invalleft(root->left, root) || invalright(root->right, root))
+        if(!isValidBST(root->left))
             return false;
         
-        return isValidBST(root->left) && isValidBST(root->right);
-    }
-    
-    int invalleft(TreeNode *node, TreeNode *root){
-        if(!node)
+        if(prev && prev->val >= root->val)
             return false;
-        while(node->right)
-            node=node->right;
-        return node->val >= root->val;
-    }
-    
-    int invalright(TreeNode *node, TreeNode *root){
-        if(!node)
-            return false;
-        while(node->left)
-            node=node->left;
-        return node->val <= root->val;
+        
+        prev = root;
+        return isValidBST(root->right);
     }
 };
