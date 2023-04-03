313ce78a5595da25cb1e43c919089694e1ef256c
Tarunbir Gambhir <tarun0131@gmail.com>
2023-04-02 21:38:46 -0700

    Time: 8 ms (89.17%), Space: 21 MB (28.94%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -14,14 +14,18 @@ public:
     bool isBalanced(TreeNode* root) {
         if(!root)
             return true;
-        int l = longest(root->left,0);
-        int r = longest(root->right,0);
-        return abs(l-r)<=1 && isBalanced(root->left) && isBalanced(root->right);
+        bool notbalnced=false;
+        longest(root,notbalnced);
+        return !notbalnced;
     }
     
-    int longest(TreeNode*node, int d){
+    int longest(TreeNode*node, bool &d){
         if(!node)
             return d;
-        return max(longest(node->left,d+1),longest(node->right,d+1));
+        int l = longest(node->left,d);
+        int r = longest(node->right,d);
+        d |= abs(l-r)>1;
+        
+        return max(l,r)+1;
     }
 };
