f2c0d52dba0a4b91fc95ffa5a68b8e2a6e183d97
Tarunbir Gambhir <tarun0131@gmail.com>
2023-04-04 11:49:03 -0700

    Time: 597 ms (77.54%), Space: 15.5 MB (68.18%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,65 +1,64 @@
 class Trie{
-    public:
-    Trie* child[26] = {};
-    bool endWord = false;
-    string word = "";
+public:
+    Trie *chars[26];
+    bool end=false;
 };
-
 class Solution {
 public:
+    Trie *root = new Trie();
+    
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Trie* root = new Trie();
-        vector<string> ans;
-        for(string s:words)
-            addWord(root,s);
+        for(string w: words)
+            addWord(w);
         
-        int m=board.size();
-        int n=board[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                findWord(board,i,j,root,ans);
+        vector<string> res;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                string tmp="";
+                dfs(board,i,j,res,root,tmp);
             }
         }
-
-        return ans;
+        
+        return res;
     }
     
-    void addWord(Trie* root, string s){
-        Trie *curr = root;
-        
-        for(char c:s){
-            if(!curr->child[c-'a'])
-                curr->child[c-'a'] = new Trie();
-            curr=curr->child[c-'a'];
-        }
-        curr->endWord = true;
-        curr->word = s;
-    }
-    
-    void findWord(vector<vector<char>> &board, int i, int j, Trie* node, vector<string> &ans){
-        
-        if(i>=board.size() || i<0 || j>=board[0].size() || j<0) return;
-        
-        char c = board[i][j];
-        
-        if(c=='*' || !node->child[c-'a'])
+    void dfs(vector<vector<char>> &b, int i, int j, vector<string> &res, Trie *node, string &tmp){
+        if(i<0 || j<0 || i>=b.size() || j>=b[0].size() || b[i][j]=='_')
             return;
         
-        node = node->child[c-'a'];
+        char c = b[i][j];
+        if(!node->chars[c-'a'])
+            return;
+
+        node = node->chars[c-'a'];
+        tmp+=c;
         
-        board[i][j] = '*';
-        
-        if(node->endWord){
-            ans.push_back(node->word);
-            node->endWord=false;
+        if(node->end){
+            res.push_back(tmp);
+            node->end = false;
         }
         
-        findWord(board, i+1, j, node, ans);
-        findWord(board, i-1, j, node, ans);
-        findWord(board, i, j+1, node, ans);
-        findWord(board, i, j-1, node, ans);
+        b[i][j] = '_';
         
-        board[i][j] = c;
-
+        dfs(b,i+1,j,res,node,tmp);
+        dfs(b,i,j+1,res,node,tmp);
+        dfs(b,i-1,j,res,node,tmp);
+        dfs(b,i,j-1,res,node,tmp);
+        
+        b[i][j]=c;
+        
+        tmp.pop_back();
+        
+    }
+    
+    void addWord(string w){
+        Trie *node = root;
+        
+        for(char c:w){
+            if(!node->chars[c-'a'])
+                node->chars[c-'a'] = new Trie();
+            node = node->chars[c-'a'];
+        }
+        node->end = true;
     }
 };
