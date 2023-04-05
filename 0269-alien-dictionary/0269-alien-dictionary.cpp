ae6b69ccecb7c48864f353b225a9fb578d5eb982
Tarunbir Gambhir <tarun0131@gmail.com>
2023-04-05 16:41:12 -0700

    Time: 0 ms (100.00%), Space: 9.5 MB (83.33%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.cpp
+++ 0269-alien-dictionary/0269-alien-dictionary.cpp
@@ -1,58 +1,61 @@
 class Solution {
 public:
-    int vis[26] = {0};
-    
     string alienOrder(vector<string>& words) {
-        map<char,vector<char>> adj;
+        unordered_map<char,vector<char>> adj;
         for(int i=0;i<words.size()-1;i++){
-            string w1 = words[i];
-            string w2 = words[i+1];
+            string w1=words[i];
+            string w2=words[i+1];
             int l = min(w1.size(),w2.size());
-            // cout<<w1.substr(0,l)<<","<<w2.substr(0,l)<<endl;
             if(w1.substr(0,l)==w2.substr(0,l) && w1.size()>w2.size())
                 return "";
             for(int i=0;i<w1.size();i++)
-                if(adj.find(w1[i])==adj.end())
+                if(adj.count(w1[i])==0)
                     adj[w1[i]].push_back({});
             for(int i=0;i<w2.size();i++)
-                if(adj.find(w2[i])==adj.end())
+                if(adj.count(w2[i])==0)
                     adj[w2[i]].push_back({});
             
-            for(int j=0;j<l;j++){
-                if(w1[j]!=w2[j]){
-                    adj[w1[j]].push_back(w2[j]);
+            for(int i=0;i<l;i++){
+                if(w1[i]!=w2[i]){
+                    adj[w1[i]].push_back(w2[i]);
                     break;
                 }
             }
         }
         if(words.size()==1){
             for(int i=0;i<words[0].size();i++)
-                if(adj.find(words[0][i])==adj.end())
+                if(adj.count(words[0][i])==0)
                     adj[words[0][i]].push_back({});
         }
-        
-        string ans;
-        for(auto letter: adj){
-            if(isCyclic(adj, letter.first, ans))
+         
+        vector<int> vis(26,0);
+        string res="";
+        for(auto x:adj)
+            if(iscyclic(adj, x.first, res, vis))
                 return "";
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        
+        // for(auto x:adj){
+        //     cout<<x.first<<":";
+        //     for(int i=0;i<x.second.size();i++)
+        //         cout<<x.second[i]<<',';
+        //     cout<<endl;
+        // }
+        reverse(begin(res),end(res));
+        return res;
     }
     
-    bool isCyclic(map<char,vector<char>> adj, char curr, string &path){
-        if(!curr)
-            return false;
-        if(vis[curr-'a']==1)
+    bool iscyclic(unordered_map<char,vector<char>> &adj, char c, string &res, vector<int> &vis){
+        if(vis[c-'a']==1)
             return true;
-        if(vis[curr-'a']==0){
-            vis[curr-'a']=1;
-            for(char next:adj[curr])
-                if(isCyclic(adj,next,path))
+        else if(vis[c-'a']==0){
+            vis[c-'a']=1;
+            for(char n: adj[c]){
+                if(adj.count(n) && iscyclic(adj,n,res,vis))
                     return true;
-                path.push_back(curr);
+            }
+            res+=c;
+            vis[c-'a']=2;
         }
-        vis[curr-'a']=2;
         return false;
     }
 };
