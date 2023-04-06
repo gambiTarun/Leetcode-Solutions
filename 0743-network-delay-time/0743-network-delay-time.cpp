300c1e2d256752936371155b4913ed3f757bb36b
Tarunbir Gambhir <tarun0131@gmail.com>
2023-04-06 13:56:50 -0700

    Time: 142 ms (40.55%), Space: 43.8 MB (25.20%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> traveltime(n,INT_MAX);
+        vector<int> traveltime(n+1,INT_MAX);
+        traveltime[k]=0;
         unordered_map<int,vector<pair<int,int>>> adj;
         for(auto t:times)
             adj[t[0]].emplace_back(t[2],t[1]);
@@ -13,20 +14,16 @@ public:
             auto x = pq.top();
             pq.pop();
             
-            traveltime[x.second-1] = min(traveltime[x.second-1],x.first);
-            
-            if(!adj.count(x.second))
-                continue;
-            
-            for(auto n:adj[x.second])
-                if(traveltime[n.second-1]==INT_MAX)
-                    pq.emplace(n.first+x.first,n.second);
+            for(auto n:adj[x.second]){
+                if(traveltime[n.second]>traveltime[x.second]+n.first){
+                    traveltime[n.second] = n.first+traveltime[x.second];
+                    pq.emplace(traveltime[n.second],n.second);
+                }
+            }
         }
         
-        for(int i=0;i<n;i++)
-            if(traveltime[i]==INT_MAX)
-                return -1;
+        int a = *max_element(traveltime.begin()+1,end(traveltime));
         
-        return *max_element(begin(traveltime),end(traveltime));
+        return a==INT_MAX?-1:a;
     }
 };
