e470b37ac47aa53fe9f6658aec437faddbd33953
Tarunbir Gambhir <tarun0131@gmail.com>
2023-04-06 16:26:19 -0700

    Time: 40 ms (47.22%), Space: 22.5 MB (77.78%) - LeetHub

---
+++ 0255-verify-preorder-sequence-in-binary-search-tree/0255-verify-preorder-sequence-in-binary-search-tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool verifyPreorder(vector<int>& preorder) {
+        stack<int> st;
+        int curmin = 0;
+        for(int x:preorder){
+            if(x<curmin)
+                return false;
+            while(!st.empty() && st.top()<x){
+                curmin = max(curmin,st.top());
+                st.pop();
+            }
+            st.push(x);
+        }
+        return true;
+    }
+};
