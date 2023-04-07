b9cc54e51ab539d3fae19f4d0c14d6c94f3f8c5e
Tarunbir Gambhir <tarun0131@gmail.com>
2023-04-06 21:43:38 -0700

    Time: 18 ms (5.57%), Space: 14.4 MB (5.34%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int,int>> q;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==2){
+                    q.emplace(i,j);
+                }
+            }
+        }
+        vector<vector<int>> dirs = {{-1,0},{0,-1},{1,0},{0,1}};
+        int res=0;
+        while(!q.empty()){
+            int s=q.size();
+            cout<<s<<endl;
+            for(int i=0;i<s;i++){
+                auto x = q.front();
+                q.pop();
+                
+                for(auto d:dirs){
+                    int r = x.first+d[0];
+                    int c = x.second+d[1];
+                    if(r>=0 && c>=0 && r<grid.size() && c<grid[0].size() && grid[r][c]==1){
+                        grid[r][c]=2;
+                        q.emplace(r,c);
+                    }
+                }
+            }
+            if(!q.empty())
+                res++;
+        }
+        
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
