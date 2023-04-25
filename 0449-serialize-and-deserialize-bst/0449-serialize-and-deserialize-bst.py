38d49bc77430d555e9cad2d83c6cb4f6d281f20d
Tarunbir Gambhir <tarun0131@gmail.com>
2023-04-25 11:38:49 -0700

    Time: 79 ms (49.93%), Space: 18.4 MB (53.32%) - LeetHub

---
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string.
+        """
+        def postorder(root):
+            return postorder(root.left) + postorder(root.right) + [root.val] if root else []
+        
+        print(' '.join(map(str, postorder(root))))
+        return ' '.join(map(str, postorder(root)))
+        
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree.
+        """
+        def create(l=float('-inf'),h=float('inf')):
+            if not data or data[-1]<l or data[-1]>h:
+                return None
+            
+            val = data.pop()
+            root = TreeNode(val)
+            root.right = create(val,h)
+            root.left = create(l,val)
+
+            return root
+        
+        data = [int(x) for x in data.split(' ') if x]
+        return create()
+            
+            
+        
+
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
